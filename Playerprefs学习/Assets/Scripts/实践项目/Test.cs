6e94fd3a9ac0145b761cf96d2a2cb731e36a8ba9
李守懿 <worang_2022@qq.com>
2025-06-05 20:03:58 +0800

    Reapply "dictionary和自定义数据类型存储"
    This reverts commit 0b0249b3d030c613a7cdc1f55c6277852f614379.

--- Playerprefs学习/Assets/Scripts/实践项目/Test.cs
+++ Playerprefs学习/Assets/Scripts/实践项目/Test.cs
@@ -1,35 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Analytics;
 
 public class PlayerInfo
 {
-    public int age = 22;
-    public string name = "李守懿";
-    public float height = 170.1f;
-    public Gender sex = Gender.Male;
+    public int age = 10;
+    public string name = "未命名";
+    public float height = 177.5f;
+    public bool sex = true;
 
-    public List<int> list = new List<int>() {1, 2, 4, 5};
-    public enum Gender
+    public List<int> list = new List<int>() { 1, 2, 3, 4 };
+
+    public Dictionary<int, string> dic = new Dictionary<int, string>()
     {
-        Male, 
-        Female  
+        { 1,"123"},
+        { 2,"234"}
+    };
+
+    public ItemInfo itemInfo = new ItemInfo(3, 99);
+
+    public List<ItemInfo> itemList = new List<ItemInfo>() {
+        new ItemInfo(1, 10),
+        new ItemInfo(2, 20),
+    };
+
+    public Dictionary<int, ItemInfo> itemDic = new Dictionary<int, ItemInfo>()
+    {
+        { 3, new ItemInfo(3, 22)},
+        { 4, new ItemInfo(4, 33)},
+    };
+}
+
+public class ItemInfo
+{
+    public int id;
+    public int num;
+
+    public ItemInfo()
+    {
+
+    }
+
+    public ItemInfo(int id, int num)
+    {
+        this.id = id;
+        this.num = num;
     }
 }
+
+
 public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        PlayerInfo palyer = new PlayerInfo();
-
-        PlayerPrefsDataMgr.Instance.SaveData(palyer, "player1");
+        PlayerInfo p = new PlayerInfo();
+        //需要把这个数据对象的信息 存储到硬盘
+        PlayerPrefsDataMgr.Instance.SaveData(p, "Player1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
